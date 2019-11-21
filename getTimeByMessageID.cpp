f4bfadc410c7328f9ac74b42fe3812644f55187a
ngk45 <tetl4pot@gmail.com>
2019-11-21 20:25:10 +0900

    add

---
+++ getTimeByMessageID.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iomanip> //fixed << setprecision(10)
+
+using namespace std;
+
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main(){
+    return 0;
+}
