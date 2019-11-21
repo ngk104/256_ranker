23c0a4acef3f48fa33eb1e9e5ffe7e280c48e201
ngk45 <tetl4pot@gmail.com>
2019-11-21 21:11:46 +0900

    上位42bitを抽出

--- getTimeByMessageID.cpp
+++ getTimeByMessageID.cpp
@@ -24,5 +24,14 @@ int main(){
     string message_id_bin = ss.str();
     cout << message_id_bin << endl;
 
+    string timestamp_bin;
+    for(int i = 0; i < 42; i++){
+        timestamp_bin[i] = message_id_bin[i];
+    }
+    for(int i = 0; i < 42; i++){
+        cout << timestamp_bin[i];
+    }
+    puts("");
+
     return 0;
 }
