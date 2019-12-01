5027cd03fdef450ee1477c0af08fea6e409af439
ngk45 <tetl4pot@gmail.com>
2019-12-01 22:31:45 +0900

    tyotto syuusei

--- discord_BOT.py
+++ discord_BOT.py
@@ -1,6 +1,5 @@
 import discord
 
-# 自分のBotのアクセストークンに置き換えてください
 TOKEN = ''
 
 # 接続に必要なオブジェクトを生成
@@ -18,9 +17,9 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    #256 wo ukete id wo kaesu
+    if message.content == '256':
+        await message.channel.send(message.id)
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
