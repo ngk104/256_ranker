3de2ea811a3687a9263f2148158e13f0f4df9c94
nngkh <tetl4pot@gmail.com>
2019-12-16 19:39:20 +0900

    commentout

--- getTimeByMessageId.cpp
+++ getTimeByMessageId.cpp
@@ -27,19 +27,19 @@ ll id2time(ll id){
     stringstream ss;
     ss << bitset<64>(message_id);
     string message_id_bin = ss.str();
-    cout << message_id_bin << endl;
+    //cout << message_id_bin << endl;
 
     char timestamp_bin[42];
     for(int i = 0; i < 42; i++){
         timestamp_bin[i] = message_id_bin[i];
     }
-    cout << timestamp_bin << endl;
+    //cout << timestamp_bin << endl;
     
     long timestamp = strtol(timestamp_bin, NULL, 2);
-    cout << timestamp << endl;
+    //cout << timestamp << endl;
 
     ll timestamp_ms = ll(timestamp + 1420070400000);
-    cout << timestamp_ms << endl;
+    //cout << timestamp_ms << endl;
 
     int unix_timestamp_ms = timestamp_ms % 1000;
     time_t unix_timestamp = timestamp_ms / 1000;
